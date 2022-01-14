befe55cab110eceb41b80ed6a4f01561375d5d18
Patrick Mamaid <patrickmamaid@gmail.com>
2022-01-14 11:41:28 -0800

    added more documents and cleanup

---
+++ win32terminalemulation1/winmenu2.h
@@ -0,0 +1,16 @@
+#define IDM_TX			100
+#define IDM_RX			101
+#define IDM_Port		102
+#define IDM_Speed		103
+#define IDM_Word		104
+#define IDM_ZETA		105
+#define IDM_Save		106
+#define IDM_THETA		107
+#define IDM_ABOUT		108
+#define IDM_COM1		109
+#define IDM_COM2		110
+#define IDM_9600		111
+#define IDM_32000		112
+#define IDM_110000		113
+#define IDM_EXIT_TO_COMMAND_PROMPT 114
+#define IDM_EXIT		115
